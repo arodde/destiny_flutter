cf8b21a520de835cc6109b83083bd869a4579995
arodde <roddealain@gmail.comgit config --global user.email roddealain@gmail.comgit config --global user.email roddealain@gmail.com>
2021-06-30 16:15:26 +0200

    step 8

--- lib/story.dart
+++ lib/story.dart
@@ -4,14 +4,25 @@ class Story {
     A. storyTitle to store the story text.
     B. choice1 to store the text for choice 1,
     C. choice2 to store the text for choice 2. */
-  String storyTitle;
-  String choice1;
-  String choice2;
+  String _storyTitle = '';
+  String _choice1 = '';
+  String _choice2 = '';
 
 //TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
   Story(
-      {required this.storyTitle, required this.choice1, required this.choice2});
-  void pouet() {
-    print('$storyTitle $choice2, $choice1');
+      {required String storyTitle,
+      required String choice1,
+      required String choice2}) {
+    this._storyTitle = storyTitle;
+    this._choice1 = choice1;
+    this._choice2 = choice2;
   }
+  // Story() : String   storyTitle, String  choice1 , String  choice2{
+  //   this._storyTitle = storyTitle;
+  //   this._choice1 = choice1;
+  //   this._choice2 = choice2;
+  // }
+  // void pouet() {
+  // print('$storyTitle $choice2, $choice1');
+  // }
 }
